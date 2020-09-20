76a82744cd04213fcbdb12e48d5ff0767d4f1793
Stephen Guilmartin <65271593+stepheng123@users.noreply.github.com>
2020-09-21 00:38:39 +0800

    updated files

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "25e9792db962de68eab2398e63d1cc02"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBA0QUwJDUlBvAa1Ex2Cd6l-UjtsvKAJqg"
